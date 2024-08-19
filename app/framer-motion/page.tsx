1327fd2dcb28fb0e6d08f087d906db5ff1544da7
KiranNepali <itsmekiran44@gmail.com>
2024-08-19 20:18:39 +0545

    init

---
+++ app/framer-motion/page.tsx
@@ -0,0 +1,12 @@
+import ParallexSectionTransition from "@/components/framer-motion/ParallexSectionTransition";
+import React from "react";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <div>
+      <ParallexSectionTransition />
+    </div>
+  );
+}
---
+++ components/framer-motion/ParallexSectionTransition.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function ParallexSectionTransition({}: Props) {
+  return <div>ParallexSectionTransition</div>;
+}
